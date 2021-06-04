54357b9b32b3dee3e5145293001afb136f7fedd0
Nidia <nidiamarquez1306@hotmail.com>
2021-06-03 20:27:19 -0500

    Ejercicio 005 Condicionales

---
+++ PYTHON/ejercicio005.py
@@ -0,0 +1,8 @@
+#si eres mayor de edad
+edad = int(input("Ingrese su Edad: "))
+if edad > 64 :
+    print ("Eres Anciano")
+elif edad >= 18 :
+    print("Usted es mayor de edad")
+else :
+    print ("Usted es menor de edad")
