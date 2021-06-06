42af3162bdb89f2645d8ac42d0793c086edd9019
Nidia <nidiamarquez1306@hotmail.com>
2021-06-06 14:09:25 -0500

    Area y Perimetro del Círculo

--- PYTHON/ejercicio004.py
+++ PYTHON/ejercicio004.py
@@ -1,1 +1,9 @@
 #Area y perimetro del circulo
+pi = 3.1416
+radio = float(input("Ingrese el radio del círculo : "))
+#Operaciones
+perimetro = 2 * pi * radio
+area = pi * (radio**2)
+#Resultados
+print("El Perimetro del Círculo: ", perimetro)
+print("El Area del Círculo: ", area)
