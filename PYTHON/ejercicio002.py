449c1d36354f6295d92449e221665431917decc6
Nidia <nidiamarquez1306@hotmail.com>
2021-06-06 14:12:48 -0500

    Area y Perimetro del Cuadrado

--- PYTHON/ejercicio002.py
+++ PYTHON/ejercicio002.py
@@ -1,12 +1,9 @@
 #Calcular el area y el perimetro del cuadrado
 #OJO ingresar valores de punto flotante.
-
 lado = input("Ingrese el valor de un lado del cuadrado: ")
-
 #Operaciones
 perimetro = float(lado) * 4
 area = float(lado) * float(lado) 
-
 #Resultados del area y perimetro
 print("El perimetro del cuadrado es:")
 print (perimetro) 
