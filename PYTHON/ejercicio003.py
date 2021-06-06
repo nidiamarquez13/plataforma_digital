bdb00a43860996b4fdbf6607a19adc02e26de2c1
Nidia <nidiamarquez1306@hotmail.com>
2021-06-06 14:28:09 -0500

    Area y Perimetro del Triángulo

--- PYTHON/ejercicio003.py
+++ PYTHON/ejercicio003.py
@@ -1,4 +1,15 @@
-#esto es una prueba git
-#Area y perimetro del triangulo
+#Area y perimetro del triangulo 
+base = float(input("Ingrese la base del Triángulo: "))
+altura = float(input("Ingrese la altura del Triángulo: "))
+lado = float(input("Ingrese el lado del Triángulo: "))
+#Operaciones
+perimetro = lado + lado + base
+area = (base * altura)/2
+#Resultados
+print("El Perimetro del Triángulo: ", perimetro)
+print("El Area del Triángulo: ", area)
+
+
+
 
 
