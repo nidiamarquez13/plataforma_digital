e56cb6d39674bbaa55fd99a91c11a86f2883e6b9
Nidia <nidiamarquez1306@hotmail.com>
2021-06-07 21:48:51 -0500

    Operadores Matemáticos

---
+++ PYTHON/ejercicio007.py
@@ -0,0 +1,22 @@
+"""
+Ejercicio de Operadores Matemáticos
+"""
+numero1 = 5
+numero2 = 2
+
+exponente = 5 ** 2 # 25
+suma = 5 + 2 # 7
+resta = 5 - 2 # 3
+multiplicacion = 5 * 2 # 10
+division = 5 / 2 # 2.5
+division_entero = 5 // 2 # 2
+modulo = 5 % 2 # 1
+
+print (exponente)
+print (suma)
+print (resta) 
+print (multiplicacion)
+print (division) 
+print (division_entero) 
+print (modulo)
+
