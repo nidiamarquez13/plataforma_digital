8b86d384f88c84b94572e20b8e2f1ce106f5372c
Nidia <nidiamarquez1306@hotmail.com>
2021-06-07 21:56:08 -0500

    Variaciones de las String

---
+++ PYTHON/ejercicio008.py
@@ -0,0 +1,17 @@
+"""
+STRING y mas!
+"""
+esto_es_una_string = "Hola"
+esto_es_otra_string = "Mundo"
+
+#Concatenar
+print(esto_es_una_string +' '+ esto_es_otra_string)
+
+#hola mundo
+
+#MAYUS
+print (esto_es_una_string.upper())
+#minus
+print (esto_es_una_string.lower())
+#Capital
+print (esto_es_una_string.capitalize())
